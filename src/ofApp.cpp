d8cc5fec0c5c2fadd2c3b161120e5231767f671a
Yiran Lu <19369063+yiranlus@users.noreply.github.com>
2025-11-04 18:25:21 +0100

    add keyboard sound

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -2,12 +2,91 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
+    ofBackground(34, 34, 34);
 
+	int bufferSize		= 128;
+	sampleRate 			= 44100;
+	phase 				= 0;
+	phaseAdder 			= 0.0f;
+	phaseAdderTarget 	= 0.0f;
+	volume				= 0.1f;
+	// bNoise 				= false;
+
+	lAudio.assign(bufferSize, 0.0);
+	rAudio.assign(bufferSize, 0.0);
+
+	soundStream.printDeviceList();
+
+	ofSoundStreamSettings settings;
+
+	// if you want to set the device id to be different than the default:
+	//
+	//	auto devices = soundStream.getDeviceList();
+	//	settings.setOutDevice(devices[3]);
+
+	// you can also get devices for an specific api:
+	//
+	//	auto devices = soundStream.getDeviceList(ofSoundDevice::Api::PULSE);
+	//	settings.setOutDevice(devices[0]);
+
+	// or get the default device for an specific api:
+	//
+	// settings.api = ofSoundDevice::Api::PULSE;
+
+	// or by name:
+	//
+	//	auto devices = soundStream.getMatchingDevices("default");
+	//	if(!devices.empty()){
+	//		settings.setOutDevice(devices[0]);
+	//	}
+
+
+	// Latest linux versions default to the HDMI output
+	// this usually fixes that. Also check the list of available
+	// devices if sound doesn't work
+
+	//settings.setApi(ofSoundDevice::MS_ASIO);
+	//settings.setApi(ofSoundDevice::MS_WASAPI);
+	//settings.setApi(ofSoundDevice::MS_DS);
+
+	auto devices = soundStream.getMatchingDevices("default");
+	if(!devices.empty()){
+		settings.setOutDevice(devices[0]);
+	}
+
+	settings.setOutListener(this);
+	settings.sampleRate = sampleRate;
+	settings.numOutputChannels = 2;
+	settings.numInputChannels = 0;
+	settings.bufferSize = bufferSize;
+	soundStream.setup(settings);
+
+	// on OSX: if you want to use ofSoundPlayer together with ofSoundStream you need to synchronize buffersizes.
+	// use ofFmodSetBuffersize(bufferSize) to set the buffersize in fmodx prior
+	// to loading a file.
+
+    // setup the keyboard mapping
+    init_mapped_frequencies();
+}
+
+//--------------------------------------------------------------
+void ofApp::init_mapped_frequencies(){
+    mappedFrequency['s'] = 261.63f; // C
+    mappedFrequency['e'] = 277.18f; // C#
+    mappedFrequency['d'] = 293.66f; // D
+    mappedFrequency['r'] = 311.13f; // D#
+    mappedFrequency['f'] = 329.63f; // E
+    mappedFrequency['g'] = 349.23f; // F
+    mappedFrequency['y'] = 369.99f; // F#
+    mappedFrequency['h'] = 392.00f; // G
+    mappedFrequency['u'] = 415.30f; // G#
+    mappedFrequency['j'] = 440.00f; // A
+    mappedFrequency['i'] = 466.16f; // A#
+    mappedFrequency['k'] = 493.88f; // B
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-
 }
 
 //--------------------------------------------------------------
@@ -16,66 +95,36 @@ void ofApp::draw(){
 }
 
 //--------------------------------------------------------------
-void ofApp::exit(){
-
-}
-
-//--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-
+    if (mappedFrequency.find(key) == mappedFrequency.end())
+        return;
+    pressedKeys.insert(key);
+    freqPhases[key] = 0.0f;
+    freqPhaseAdders[key] = (mappedFrequency[key] / (float) sampleRate) * glm::two_pi<float>();
 }
 
 //--------------------------------------------------------------
 void ofApp::keyReleased(int key){
+    pressedKeys.erase(key);
+    freqPhases.erase(key);
 
+    if (pressedKeys.empty()) {
+        phase = 0.0f;
+        phaseAdderTarget = 0.0f;
+    }
 }
 
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
+void ofApp::audioOut(ofSoundBuffer & buffer){
+	// sin (n) seems to have trouble when n is very large, so we
+	// keep phase in the range of 0-glm::two_pi<float>() like this:
 
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseScrolled(int x, int y, float scrollX, float scrollY){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
+    for (size_t i = 0; i < buffer.getNumFrames(); i++){
+        float sample = 0.0f;
+        for (const auto &key: pressedKeys) {
+            freqPhases[key] = fmod(freqPhases[key] + freqPhaseAdders[key], glm::two_pi<float>());
+            sample += sin(freqPhases[key]) / pressedKeys.size();
+        }
+        lAudio[i] = buffer[i*buffer.getNumChannels()    ] = sample * volume;
+        rAudio[i] = buffer[i*buffer.getNumChannels() + 1] = sample * volume;
+    }
 }
--- src/ofApp.h
+++ src/ofApp.h
@@ -1,5 +1,15 @@
 #pragma once
 
+#include <vector>
+#include <set>
+#include <map>
+using std::vector;
+using std::set;
+using std::map;
+
+#include <iostream>
+using std::cout;
+
 #include "ofMain.h"
 
 class ofApp : public ofBaseApp{
@@ -8,19 +18,38 @@ class ofApp : public ofBaseApp{
 		void setup() override;
 		void update() override;
 		void draw() override;
-		void exit() override;
+		// void exit() override;
 
 		void keyPressed(int key) override;
 		void keyReleased(int key) override;
-		void mouseMoved(int x, int y ) override;
-		void mouseDragged(int x, int y, int button) override;
-		void mousePressed(int x, int y, int button) override;
-		void mouseReleased(int x, int y, int button) override;
-		void mouseScrolled(int x, int y, float scrollX, float scrollY) override;
-		void mouseEntered(int x, int y) override;
-		void mouseExited(int x, int y) override;
-		void windowResized(int w, int h) override;
-		void dragEvent(ofDragInfo dragInfo) override;
-		void gotMessage(ofMessage msg) override;
-		
+		// void mouseMoved(int x, int y ) override;
+		// void mouseDragged(int x, int y, int button) override;
+		// void mousePressed(int x, int y, int button) override;
+		// void mouseReleased(int x, int y, int button) override;
+		// void mouseScrolled(int x, int y, float scrollX, float scrollY) override;
+		// void mouseEntered(int x, int y) override;
+		// void mouseExited(int x, int y) override;
+		// void windowResized(int w, int h) override;
+		// void dragEvent(ofDragInfo dragInfo) override;
+		// void gotMessage(ofMessage msg) override;
+
+		void init_mapped_frequencies();
+		void audioOut(ofSoundBuffer & buffer) override;
+
+	private:
+
+		ofSoundStream soundStream;
+		int sampleRate;
+		float phase;
+		float phaseAdder;
+		float phaseAdderTarget;
+
+		set<int> pressedKeys;
+		map<int, float> mappedFrequency;
+		map<int, float> freqPhases;
+		map<int, float> freqPhaseAdders;
+
+		vector<float> lAudio;
+		vector<float> rAudio;
+		float volume;
 };

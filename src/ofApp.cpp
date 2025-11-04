99676ceed438ad26fbba150dbd8dcb989ab8c51a
Julien D. <julienhm.dubois@gmail.com>
2025-11-04 17:00:19 +0100

    feat: show 12-key keyboard

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -2,7 +2,67 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
+ofBackground(50, 50, 50);
+	
+	int bufferSize		= 512;
+	sampleRate 			= 44100;
+	phase 				= 0;
+	phaseAdder 			= 0.0f;
+	phaseAdderTarget 	= 0.0f;
+	volume				= 0.1f;
+	bNoise 				= false;
 
+	lAudio.assign(bufferSize, 0.0);
+	rAudio.assign(bufferSize, 0.0);
+	
+	soundStream.printDeviceList();
+
+	ofSoundStreamSettings settings;
+
+	// if you want to set the device id to be different than the default:
+	//
+	//	auto devices = soundStream.getDeviceList();
+	//	settings.setOutDevice(devices[3]);
+
+	// you can also get devices for an specific api:
+	//
+	//	auto devices = soundStream.getDeviceList(ofSoundDevice::Api::PULSE);
+	//	settings.setOutDevice(devices[0]);
+
+	// or get the default device for an specific api:
+	//
+	// settings.api = ofSoundDevice::Api::PULSE;
+
+	// or by name:
+	//
+	//	auto devices = soundStream.getMatchingDevices("default");
+	//	if(!devices.empty()){
+	//		settings.setOutDevice(devices[0]);
+	//	}
+
+
+	// Latest linux versions default to the HDMI output
+	// this usually fixes that. Also check the list of available
+	// devices if sound doesn't work
+
+	//settings.setApi(ofSoundDevice::MS_ASIO);
+	//settings.setApi(ofSoundDevice::MS_WASAPI);
+	//settings.setApi(ofSoundDevice::MS_DS);
+
+	auto devices = soundStream.getMatchingDevices("default");
+	if(!devices.empty()){
+		settings.setOutDevice(devices[0]);
+	}
+
+
+
+
+	settings.setOutListener(this);
+	settings.sampleRate = sampleRate;
+	settings.numOutputChannels = 2;
+	settings.numInputChannels = 0;
+	settings.bufferSize = bufferSize;
+	soundStream.setup(settings);
 }
 
 //--------------------------------------------------------------
@@ -11,8 +71,45 @@ void ofApp::update(){
 }
 
 //--------------------------------------------------------------
-void ofApp::draw(){
+void ofApp::draw()
+{
+    ofSetColor(225);
+    ofDrawBitmapString("SUPER MEGA SYNTHESIZER OF THE DEAD", 50, 50);
 
+    // Setting keyboard properties
+    int x_keyboard = 200;
+    int y_keyboard = 800;
+    int key_width = 40;
+    int padding = 4;
+    int rounding = 5;
+
+    // Drawing white keys
+    ofSetColor(255,255,255);
+    ofFill();
+    for (int i = 0; i < 7; i++)
+    {
+        ofDrawRectRounded(
+            x_keyboard + (i * (key_width + padding)), 
+            y_keyboard, 
+            key_width, 
+            5 * key_width, 
+            rounding);
+    }
+    
+    // Drawing black keys
+    ofSetColor(15,15,15);
+    for (int i = 0; i < 6; i++)
+    {
+        if (i != 2)
+        {
+            ofDrawRectRounded(
+                x_keyboard + (1.5 * key_width + padding)/2 + (i * (key_width + padding)),
+                y_keyboard, 
+                key_width / 2, 
+                7 * key_width / 2,
+                rounding);
+        }
+    }
 }
 
 //--------------------------------------------------------------
--- src/ofApp.h
+++ src/ofApp.h
@@ -22,5 +22,21 @@ class ofApp : public ofBaseApp{
 		void windowResized(int w, int h) override;
 		void dragEvent(ofDragInfo dragInfo) override;
 		void gotMessage(ofMessage msg) override;
+
+		ofSoundStream soundStream;
+
+		float 	pan;
+		int		sampleRate;
+		bool 	bNoise;
+		float 	volume;
+
+		vector <float> lAudio;
+		vector <float> rAudio;
+		
+		//------------------- for the simple sine wave synthesis
+		float 	targetFrequency;
+		float 	phase;
+		float 	phaseAdder;
+		float 	phaseAdderTarget;
 		
 };
